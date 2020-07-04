8077c94c689d86666564f9e37cb3b64f886a5042
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2020-07-04 16:26:23 +0900

    2020-07-04-1600

--- src/servlet/SendMailServlet.java
+++ src/servlet/SendMailServlet.java
@@ -2,7 +2,6 @@ package servlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import DAO.RegisterDAO;
-import model.CommonsMail;
 import model.User;
 
 
@@ -20,9 +18,6 @@ public class SendMailServlet extends HttpServlet {
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-
-      request.setCharacterEncoding("UTF-8");
-      //Apache24/htdocs/index.htmlから何の情報を受け取るかを記述
 
       HttpSession session = request.getSession();
       User user = (User) session.getAttribute("user");
@@ -45,38 +40,6 @@ public class SendMailServlet extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
 
-    request.setCharacterEncoding("UTF-8");
-    String name = request.getParameter("name");
-    String pass = request.getParameter("pass");
-    String mail = request.getParameter("mail");
-
-    //---- input each parameter to user ----
-    User user = new User(name, pass, mail);
-
-    //---- set user to session scope ----
-    HttpSession session = request.getSession();
-    session.setAttribute("user", user);
-
-    //---- call CommonsMail() ----
-    CommonsMail commonsMail = new CommonsMail();
-
-    String toMail = commonsMail.buildMail(user);
-
-    String subject = "Verify Mail";
-    String message = "http://localhost:80?name=" + name + "&mail=" + toMail;
-
-    //EncodeURL encodeURL = new EncodeURL();
-    //String encodeMessage = encodeURL.toUniCode(message);
-    //---- send mail ----
-    boolean doneMail = commonsMail.send(subject, message, toMail);
-
-    //---- set result of if done mail to request scope ----
-    request.setAttribute("doneMail", doneMail);
-
-    //---- forward to registerResult ----
-    String path = "/mailDone.jsp";
-    RequestDispatcher dis = request.getRequestDispatcher(path);
-    dis.forward(request, response);
 
   }//doPost()
 
