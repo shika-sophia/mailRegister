a4c1a7d229f53a43194daef062e179ea404a5c88
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2020-07-05 21:17:37 +0900

    2020-07-05-2100

--- WebContent/Apache24_htdocs/for_apache.js
+++ WebContent/Apache24_htdocs/for_apache.js
@@ -1,7 +1,7 @@
 /**
  *for Apache/htdocs/index.html
  */
-
+/*
 function getUrlQuery() {
   var queryStr = window.location.search.slice(1);  // 文頭?を除外
       query = {};
@@ -17,6 +17,29 @@ function getUrlQuery() {
     var queryArray = queryStr.split('=');
     query[queryArray[0]] = queryArray[1];
   });
-
   return query;
-}
+}//function
+*/
+
+function getUrlQuery(){
+    var queryStr = window.location.search.substring(1);
+    var queryArray = [];
+
+    if (queryStr){
+        var division = queryStr.split('&');
+
+        for (i = 0; i < division.length; i++){
+            var reDivision = division[i].split('=');
+            queryArray[reDivision[0]] = reDivision[1];
+        }//for i
+
+    } else if (!queryStr) {
+        queryArray['name'] = "";
+        queryArray['pass'] = "";
+        queryArray['mail'] = "";
+    }//if
+
+    return queryArray;
+
+}//function getUrlQuery()
+
