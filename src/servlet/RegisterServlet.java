684d470a51d118e8eac2b5e4f82ea180e5c7265c
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2020-07-05 21:19:18 +0900

    2020-07-05-2100

--- src/servlet/RegisterServlet.java
+++ src/servlet/RegisterServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import model.CommonsMail;
-import model.EnCodeURL;
 import model.RegisterLogic;
 import model.User;
 
@@ -80,21 +79,14 @@ public class RegisterServlet extends HttpServlet {
           //---- no existMail -> send Mail ----
           //---- call CommonsMail() ----
           CommonsMail commonsMail = new CommonsMail();
-          String toMail = commonsMail.buildMail(user);
+          StringBuilder mailMessage = commonsMail.buildMail(user);
 
-          EnCodeURL enCodeUrl = new EnCodeURL();
-          StringBuilder passCode = enCodeUrl.buildPassCode(pass);
 
-          String subject = "Verify Mail";
-          StringBuilder mailMessage = new StringBuilder();
-                  mailMessage.append("http://localhost:80?name=").append(name);
-                  mailMessage.append("&pass=").append(passCode);
-                  mailMessage.append("&mail=").append(toMail);
-
-          //EncodeURL encodeURL = new EncodeURL();
+          //EnCodeURL encodeUrl = new EnCodeURL();
           //String encodeMessage = encodeURL.toUniCode(mailMessage);
+
           //---- send mail ----
-          boolean doneMail = commonsMail.send(subject, mailMessage, toMail);
+          boolean doneMail = commonsMail.send(mailMessage);
 
           //---- set result of if mail was done,to request scope ----
           request.setAttribute("doneMail", doneMail);
