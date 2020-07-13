abaf900bf735418ad172900f3e96748057875e6e
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2020-07-13 10:05:13 +0900

    2020-07-13-1000
    added Function of DELETE tempRegister over 24hours without Server Test, DB Test.

--- src/model/NowDateTime.java
+++ src/model/NowDateTime.java
@@ -1,26 +1,65 @@
 package model;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class NowDateTime {
 
-	//public static void main(String[] args) {
-	public String nowDateTime() {
+    //public static void main(String[] args) {
+    public String nowDateTime() {
 
-		LocalDateTime ldt = LocalDateTime.now();
-		String nowDateTime = ldt.format(DateTimeFormatter.
-			ofPattern("Y年M月d日(E) h時m分s秒"));
+        LocalDateTime now = LocalDateTime.now();
 
-		//System.out.println(nowDateTime);
+        //---- Formatter (see Result following) ----
+        //String nowDateTime = ldt.format(DateTimeFormatter.
+        //	ofPattern("Y年M月d日(E) h時m分s秒"));
 
-		return nowDateTime;
+        //---- Format for MySQL as like 'YYYY-MM-DD hh:mm:ss' ----
+        String nowStr = now.toString().substring(0, 19); // "."以下を除去
+        String[] division = nowStr.toString().split("T");// "T"で分割
+        String nowDateTime = String.join(" ", division);// " "で再結合
 
-	}
+        //System.out.println(now);
+        //System.out.println(nowDateTime);
 
-}
+        return nowDateTime;
 
-/*結果
+    }//nowDateTime() or main()
+
+    public String over24hDateTime() {
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime over24h = now.plus(24, ChronoUnit.HOURS);
+
+        //---- Formatter (see Result following) ----
+        //String nowDateTime = ldt.format(DateTimeFormatter.
+        //	ofPattern("Y年M月d日(E) h時m分s秒"));
+
+        //---- Format for MySQL as like 'YYYY-MM-DD hh:mm:ss' ----
+        String over24hStr = over24h.toString().substring(0, 19); // "."以下を除去
+        String[] division = over24hStr.toString().split("T");// "T"で分割
+        String over24hDateTime = String.join(" ", division);// " "で再結合
+
+        //System.out.println(now);
+        //System.out.println(over24hDateTime);
+
+        return over24hDateTime;
+
+    }//Over24hDateTime()
+
+}//class
+
+
+/*
+//====== Result / 結果 ======
 LocalDateTime:2020-07-07T12:04:25.180645500
 LocalDateTimeFormatter:2020年7月7日(火) 11時57分40秒
+
+//====== Result after remake DB from varchar(40) to DateTime ======
+LocalDateTime: 2020-07-13T08:47:30.169852200
+nowDateTime:   2020-07-13 08:47:30
+
+//====== Result of 'over24h' ======
+LocalDateTime.now: 2020-07-13T09:33:38.926300300
+over24hDateTime:   2020-07-14 09:33:38
 */
--- src/model/TempRegisterLogic.java
+++ src/model/TempRegisterLogic.java
@@ -9,17 +9,22 @@
 
 package model;
 
+import java.util.List;
+
 import DAO.TempRegisterDAO;
 
 public class TempRegisterLogic {
+    //---- field definition ----
     TempRegisterDAO dao = new TempRegisterDAO();
+    NowDateTime ndt = new NowDateTime();
 
     //====== get nowDateTime and call DAO for Temporary Register ======
     public boolean executeTempRegister(User user) {
 
-        NowDateTime ndt = new NowDateTime();
+        //---- get nowDateTime for tempRegister ----
         String nowDateTime = ndt.nowDateTime();
 
+        //---- call DAO for tempRegister ----
         boolean doneTempRegister = dao.doneTempRegister(user,nowDateTime);
 
         return doneTempRegister;
@@ -40,7 +45,21 @@ public class TempRegisterLogic {
         User user = dao.getPastUser(mailCode);
 
         return user;
-    }
+    }//getPastUser()
+
+
+    //====== Logic to delete Temporary Register when be over 24hours ======
+    //【未実装】まだServer Test, DB Testをしていないので、ここを呼び出すServletの記述なし
+    public boolean tempDeleteOver24h () {
+        //---- get nowDateTime for tempRegister ----
+        String over24hDateTime = ndt.over24hDateTime();
+
+        //---- call DAO for select rows over 24h ----
+        List<String> over24hList= dao.selectOver24h(over24hDateTime);
+        boolean beClear = dao.tempDeleteOver24h(over24hList);
+
+        return beClear;
+    }//tempDeleteOver24h()
 }//class
 
 
